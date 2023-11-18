595e93f7a7126061a342327c378f29282bfe309e
Offuru <andrei.rozmarin@gmail.com>
2023-11-18 19:39:04 +0200

    Added moveToNext() method

--- Twixt/DebuilderBob.cpp
+++ Twixt/DebuilderBob.cpp
@@ -1,10 +1,10 @@
 #include "DebuilderBob.h"
 
-DebuilderBob::DebuilderBob(uint8_t boardSize)
+DebuilderBob::DebuilderBob(Board& board) : m_board{ board }, m_lastPosition{ {} }
 {
 	std::random_device rd;
 	std::mt19937 gen(rd());
-	std::uniform_int_distribution<> distrib(0, boardSize - 1);
+	std::uniform_int_distribution<> distrib(0, m_board.get().getSize() - 1);
 	m_position = { distrib(gen),  distrib(gen) };
 }
 
@@ -15,5 +15,25 @@ Position DebuilderBob::getPosition() const
 
 void DebuilderBob::setPosition(const Position& position)
 {
+	m_position = position;
+}
+
+void DebuilderBob::moveToNext()
+{
+	std::random_device rd;
+	std::mt19937 gen(rd());
+	std::uniform_int_distribution<> distrib(0, m_board.get().getSize() - 1);
+	Position position;
+
+	while (true)
+	{
+		position = { distrib(gen),  distrib(gen) };
+		
+		if (!m_board.get()[position].getMined() && !(m_lastPosition.has_value() && m_lastPosition.value() == position))
+			break;
+	}
+
+	//TO DO: destroy pylon from board if on foundation
+	m_lastPosition = m_position;
 	m_position = position;
 }
--- Twixt/DebuilderBob.h
+++ Twixt/DebuilderBob.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <utility>;
 #include <random>;
+#include <optional>
 #include "Board.h";
 using Position = std::pair<uint8_t, uint8_t>;
 
@@ -8,9 +9,11 @@ class DebuilderBob
 {
 private:
 	Position m_position;
+	std::optional<Position> m_lastPosition;
+	std::reference_wrapper<Board> m_board;
 
 public:
-	DebuilderBob(uint8_t = 24);
+	DebuilderBob(Board&);
 	DebuilderBob(const DebuilderBob&) = default;
 	~DebuilderBob() = default;
 	DebuilderBob& operator=(const DebuilderBob&) = default;
@@ -18,4 +21,5 @@ public:
 	Position getPosition() const;
 	void setPosition(const Position&);
 
+	void moveToNext();
 };
