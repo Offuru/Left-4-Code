26b78cae91bad8fcadbc130e0fecb8c560b119d0
Offuru <andrei.rozmarin@gmail.com>
2023-11-28 21:08:00 +0200

    Added setters and getters for name

--- Twixt/IPlayer.cpp
+++ Twixt/IPlayer.cpp
@@ -1,3 +1,71 @@
 #include "IPlayer.h"
 
-using namespace twixt;
+twixt::IPlayer::IPlayer(const std::string& name)
+{
+	m_name = name;
+	m_color = Pylon::Color::Red;
+	m_noBridges = 50;
+	m_noPylons1x1 = 30;
+	m_noPylons2x2 = 5;
+	m_noPylonsCross = 5;
+}
+
+void twixt::IPlayer::setName(const std::string& name)
+{
+	m_name = name;
+}
+
+void twixt::IPlayer::setColor(Pylon::Color color)
+{
+	m_color = color;
+}
+
+void twixt::IPlayer::setNoBridges(uint8_t noBridges)
+{
+	m_noBridges = noBridges;
+}
+
+void twixt::IPlayer::setNoPylons1x1(uint8_t noPylons)
+{
+	m_noPylons1x1 = noPylons;
+}
+
+void twixt::IPlayer::setNoPylons2x2(uint8_t noPylons)
+{
+	m_noPylons2x2 = noPylons;
+}
+
+void twixt::IPlayer::setNoPylonsCross(uint8_t noPylons)
+{
+	m_noPylonsCross = noPylons;
+}
+
+std::string twixt::IPlayer::getName() const
+{
+	return m_name;
+}
+
+twixt::Pylon::Color twixt::IPlayer::getColor() const
+{
+	return m_color;
+}
+
+uint8_t twixt::IPlayer::getNoBridges() const
+{
+	return m_noBridges;
+}
+
+uint8_t twixt::IPlayer::getNoPylons1x1() const
+{
+	return m_noPylons1x1;
+}
+
+uint8_t twixt::IPlayer::getNoPylons2x2() const
+{
+	return m_noPylons2x2;
+}
+
+uint8_t twixt::IPlayer::getNoPylonsCross() const
+{
+	return m_noPylonsCross;
+}
--- Twixt/IPlayer.h
+++ Twixt/IPlayer.h
@@ -3,6 +3,7 @@
 #include <tuple>
 #include <optional>
 #include <string_view>
+#include "Utilities.h"
 #include "Pylon.h"
 
 namespace twixt
@@ -22,10 +23,10 @@ namespace twixt
 
 		};
 
-		using Position = ::std::pair<uint8_t, uint8_t>;
-		using Move = ::std::tuple<Action, Position, ::std::optional<Position>>;
+		// optional position in case of add/remove bridge action
+		using Move = std::tuple<Action, Position, std::optional<Position>>;
 
-		IPlayer(const ::std::string& name = "");
+		IPlayer(const std::string& name = "");
 
 		IPlayer(const IPlayer&) = default;
 		IPlayer(IPlayer&&) = default;
@@ -35,13 +36,17 @@ namespace twixt
 
 
 		virtual Move getNextMove() = 0;
+		bool validMove(const Move&) const;//TO DO: define to check 
+										  //whether or not move was valid (position, color, noPieces, etc)
 
+		void setName(const std::string&);
 		void setColor(Pylon::Color);
 		void setNoBridges(uint8_t);
 		void setNoPylons1x1(uint8_t);
 		void setNoPylons2x2(uint8_t);
 		void setNoPylonsCross(uint8_t);
 
+		std::string getName() const;
 		Pylon::Color getColor() const;
 		uint8_t getNoBridges() const;
 		uint8_t getNoPylons1x1() const;
@@ -50,8 +55,8 @@ namespace twixt
 
 	protected:
 
-		::std::string name;
-		Pylon::Color m_color : 1;
+		std::string m_name;
+		Pylon::Color m_color : 2;
 		uint8_t m_noBridges;
 		uint8_t m_noPylons1x1;
 		uint8_t m_noPylons2x2;
