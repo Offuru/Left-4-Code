ee2c2d13b113ef94b8b5b5f1e7662c292eefe08b
Offuru <andrei.rozmarin@gmail.com>
2023-11-28 21:09:28 +0200

    Tested Run() method for game class

--- Twixt/main.cpp
+++ Twixt/main.cpp
@@ -6,32 +6,18 @@ using namespace twixt;
 int main()
 {
 	Game test;
+
 	test.setReusableMinedFoundation(true);
 	test.setExplodeSingleLocation(true);
 	test.setExplodeCol(true);
-	test.setExplodeRow(true);
+	test.setExplodeRow(false);
 	test.setExplodeArea(true);
+	test.setDebuilderBob(true);
+	test.setReusableMinedFoundation(true);
 	test.getBoard().spawnMines();
-	test.addPylon(::std::make_pair(2, 19), Pylon::Type::Cross, Pylon::Color::Black);
-	test.addPylon(::std::make_pair(3, 13), Pylon::Type::Cross, Pylon::Color::Red);
-	test.printBoard();
-	::std::cout << "\n\n";
+
 	test.getBoard().getFoundation({ 12, 12 }).setMined(true);
-	test.printBoard();
-	::std::cout << "\n\n";
-	test.addPylon(::std::make_pair(12, 12), Pylon::Type::Cross, Pylon::Color::Red);
-	//test.addPylon(::std::make_pair(4, 5), Pylon::Type::Single, Pylon::Color::Red);
-	//test.addPylon(::std::make_pair(5, 5), Pylon::Type::Single, Pylon::Color::Red);
-	//test.addPylon(::std::make_pair(4, 6), Pylon::Type::Single, Pylon::Color::Red);
-	//test.addBridge(::std::make_pair(3, 3), ::std::make_pair(4, 5));
-	//test.addBridge(::std::make_pair(3, 4), ::std::make_pair(4, 6));
-	//test.addBridge(::std::make_pair(4, 3), ::std::make_pair(5, 5));
-	//test.getBoard().removePylon({ 2,3 });
-	//test.addPylon(::std::make_pair(3, 3), Pylon::Type::Cross, Pylon::Color::Red);
-	//test.addBridge(::std::make_pair(3, 3), ::std::make_pair(4, 5));
-	////test.moveBob();
-	//test.removeBridge( { 4,5 },{ 3,3 });
-	//test.addBridge(::std::make_pair(4, 3), ::std::make_pair(5, 5));
-	test.printBoard();
+
+	test.Run();
 	return 0;
 }
