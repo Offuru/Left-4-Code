1a7c4ea23d38ffaa20dd3409283ea663c57084d9
Constantin Voicu <voicu.constantin@outlook.com>
2023-12-18 23:22:05 +0200

    Made connections for each check box and spin box in order to set the game flags, the board size, number of small pylons and number of mines

--- GUI/SettingsWindow.cpp
+++ GUI/SettingsWindow.cpp
@@ -1,9 +1,38 @@
 #include "SettingsWindow.h"
 
-SettingsWindow::SettingsWindow(QWidget *parent)
+SettingsWindow::SettingsWindow(QWidget *parent, std::shared_ptr<twixt::Game> game)
 	: QMainWindow{ parent }, m_ui{ std::make_unique<Ui::SettingsWindowClass>() }
 {
+	setWindowFlags(Qt::Window | Qt::MSWindowsFixedSizeDialogHint);
+	m_game = game;
 	m_ui->setupUi(this);
+
+	QObject::connect(m_ui->reusableCheckBox, &QCheckBox::stateChanged,
+					this, [&]() { m_ui->reusableCheckBox->isChecked()?
+					m_game->setReusableMinedFoundation(true):m_game->setReusableMinedFoundation(false); });
+
+	QObject::connect(m_ui->columnExplotionCheckBox, &QCheckBox::stateChanged,
+					this, [&]() { m_ui->columnExplotionCheckBox->isChecked()?
+					m_game->setExplodeCol(true):m_game->setExplodeCol(false); });
+
+	QObject::connect(m_ui->rowExplotionCheckBox, &QCheckBox::stateChanged,
+					this, [&]() { m_ui->rowExplotionCheckBox->isChecked()?
+					m_game->setExplodeRow(true):m_game->setExplodeRow(false); });
+
+	QObject::connect(m_ui->areaExplotionCheckBox, &QCheckBox::stateChanged,
+					this, [&]() { m_ui->areaExplotionCheckBox->isChecked()?
+					m_game->setExplodeArea(true):m_game->setExplodeArea(false); });
+
+	QObject::connect(m_ui->minesSpinBox, &QSpinBox::valueChanged,
+					this, [&]() { m_game->getBoard().setTotalMines(m_ui->minesSpinBox->value()); });
+
+	QObject::connect(m_ui->boardSizeSpinBox, &QSpinBox::valueChanged,
+					this, [&]() { m_game->getBoard().setSize(m_ui->boardSizeSpinBox->value()); });
+
+	QObject::connect(m_ui->pylonsSpinBox, &QSpinBox::valueChanged,
+					this, 
+					[&]() { m_game->getPlayer1()->setNoPylons1x1(m_ui->pylonsSpinBox->value());
+							m_game->getPlayer2()->setNoPylons1x1(m_ui->pylonsSpinBox->value()); });
 }
 
 SettingsWindow::~SettingsWindow()
