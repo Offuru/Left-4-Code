6ce204de0defabc31375210f6e91d6b6de285ee7
Offuru <andrei.rozmarin@gmail.com>
2023-12-24 22:30:01 +0200

    Added setters, getters and enum to string hashmaps for Target and Effect

--- Twixt/Card.h
+++ Twixt/Card.h
@@ -1,8 +1,10 @@
 #pragma once
-
+#include <string>
+#include <unordered_map>
 
 class Card
 {
+public:
 	enum class Target
 	{
 		None,
@@ -24,8 +26,13 @@ class Card
 		PlaceMine
 	};
 
+private:
 	Target m_target;
 	Effect m_effect;
+
+	static std::unordered_map<Target, std::string> targets;
+	static std::unordered_map<Effect, std::string> effects;
+	//TO DO: move key_val pairs to file and add methods for reading
 
 public:
 
@@ -35,5 +42,14 @@ public:
 	Card& operator=(const Card& other);
 	Card(Card&& other) = default;
 	Card& operator=(Card&& other) = default;
+
+	void setTarget(Target target);
+	void setEffect(Effect effect);
+
+	Target getTarget() const;
+	Effect getEffect() const;
+
+	std::string getTargetString() const;
+	std::string getEffectString() const;
 };
 
