652a9597bdc642e458ca779360bc732abd93b386
dragos <dragos.stanica@student.unitbv.ro>
2024-01-09 00:41:58 +0200

    Added selection method and calculate UCB

--- Twixt/GameTree.cpp
+++ Twixt/GameTree.cpp
@@ -5,7 +5,25 @@ twixt::GameTree::GameTree(size_t depth) :
 {}
 
 twixt::GameTree::GameTree(Board rootGame, size_t depth) :
-	m_depth{ depth }, m_root{ std::move(rootGame), nullptr, NodeVec{} }
+	m_depth{ depth }, m_root{ std::move(rootGame), nullptr, NodeVec{} , 0, 0, 0}
 {}
+
+twixt::NodeRef twixt::GameTree::selection(NodeRef curr_node)
+{
+	double maxUCB = -std::numeric_limits<float>::infinity();
+	Node selected_child;
+	
+	for (auto child : curr_node.get().children)
+	{
+		float currUCB = child.UCB();
+		if (currUCB > maxUCB)
+		{
+			maxUCB = currUCB;
+			selected_child = child;
+		}
+	}
+
+	return std::reference_wrapper<Node>(selected_child);
+}
 
 
--- Twixt/GameTree.h
+++ Twixt/GameTree.h
@@ -1,6 +1,7 @@
 #pragma once
 #include "Board.h"
-
+#include <limits>
+#include <numbers>
 
 namespace twixt
 {
@@ -10,29 +11,40 @@ namespace twixt
 	using NodeRef = std::reference_wrapper<Node>;
 	using NodeVec = std::vector<Node>;
 	using NodeRefVec = std::vector<NodeRef>;
-	
+
 	struct Node
 	{
 		Board currentBoard;
 		NodePtr parent;
 		NodeVec children;
 
-		float UCB{}; //upper confidence bound
+		float winningScore{};
+		int parentVisits{};
+		int currentVisists{};
+
+		//upper confidence bound
+		double UCB() const
+		{
+			double ans = winningScore + 2 * sqrt((log(parentVisits + std::numbers::e * pow(10.0, -6.0)) / (currentVisists + pow(10., -10.))));
+			return ans;
+		}
 
 		bool isLeaf() const noexcept
 		{
 			return children.size() == 0;
 		}
 	};
-	
+
 
 	class GameTree
 	{
 	public:
-		
+
 		GameTree(size_t depth = 0u);
 
 		GameTree(Board rootGame, size_t depth = 0u);
+
+		NodeRef selection(NodeRef curr_node);
 
 	private:
 		size_t m_depth;
