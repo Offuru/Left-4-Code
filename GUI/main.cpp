61b5d63b411dc1ff8bfcd3c83048a32bc9cd843c
Constantin Voicu <voicu.constantin@outlook.com>
2024-01-14 23:12:24 +0200

    Added function to add style sheet to QApplication

--- GUI/main.cpp
+++ GUI/main.cpp
@@ -2,16 +2,23 @@
 #include <QtWidgets/QApplication>
 #include "Game.h"
 
+void addStyleToQApp(QApplication& app);
+
 int main(int argc, char *argv[])
 {
-    QApplication a(argc, argv);
+    QApplication TwixtApp(argc, argv);
+    addStyleToQApp(TwixtApp);
+
+    StartWindow window;
+    window.show();
+
+    return TwixtApp.exec();
+}
+
+void addStyleToQApp(QApplication& app)
+{
     QFile file("Static files/QSS/styleSheet.qss");
     file.open(QFile::ReadOnly);
     QString styleSheet = QLatin1String(file.readAll());
-    a.setStyleSheet(styleSheet);
-
-    StartWindow w;
-    w.show();
-
-    return a.exec();
+    app.setStyleSheet(styleSheet);
 }
